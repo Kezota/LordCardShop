f40b91c0742cd9fdc4e335801fbcf3d0518f1e4a
Kezota <keziameilany2005@gmail.com>
2025-05-31 00:57:37 +0700

    feat: added crystal report

---
+++ LordCardShop/Views/CrystalReport.aspx.cs
@@ -0,0 +1,27 @@
+﻿using LordCardShop.Dataset;
+using LordCardShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LordCardShop.Views
+{
+    public partial class CrystalReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CrystalReport report = new CrystalReport();
+            CrystalReportViewer1.ReportSource = report;
+            report.setDataSource();
+        }
+
+        private ModelDataset getData(List<TransactionDetail> transactions)
+        {
+            ModelDataset data = new ModelDataset();
+            var headerTable = data.
+        }
+    }
+}

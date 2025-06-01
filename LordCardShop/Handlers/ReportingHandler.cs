6beb0679db3ca53c6538db56cf11c7fa220ce71b
Williams <133097737+nocturnalnerds@users.noreply.github.com>
2025-06-01 19:50:42 +0700

    Crystal Report Done

---
+++ LordCardShop/Handlers/ReportingHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using LordCardShop.Dataset;
+using LordCardShop.Model;
+
+namespace LordCardShop.Handlers
+{
+    public class ReportingHandler
+    {
+        public static List<TransactionDetail> GetAllTransactions()
+        { 
+            return new List<TransactionDetail>();
+        }
+    }
+}

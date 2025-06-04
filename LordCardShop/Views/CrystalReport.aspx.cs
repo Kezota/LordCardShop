d0ef795816b018be27d4d0417bc10a8cf03d21d9
Kezota <keziameilany2005@gmail.com>
2025-06-04 18:23:09 +0700

    feat: added CR

--- LordCardShop/Views/CrystalReport.aspx.cs
+++ LordCardShop/Views/CrystalReport.aspx.cs
@@ -1,27 +1,61 @@
-﻿//using LordCardShop.Dataset;
-//using LordCardShop.Model;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.UI;
-//using System.Web.UI.WebControls;
+﻿using LordCardShop.Dataset;
+using LordCardShop.Model;
+using LordCardShop.Reporting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
-//namespace LordCardShop.Views
-//{
-//    public partial class CrystalReport : System.Web.UI.Page
-//    {
-//        protected void Page_Load(object sender, EventArgs e)
-//        {
-//            CrystalReport report = new CrystalReport();
-//            CrystalReportViewer1.ReportSource = report;
-//            report.setDataSource();
-//        }
+namespace LordCardShop.Views
+{
+    public partial class CrystalReport : System.Web.UI.Page
+    {
+        DatabaseEntities db = new DatabaseEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
 
-//        private ModelDataset getData(List<TransactionDetail> transactions)
-//        {
-//            ModelDataset data = new ModelDataset();
-//            var headerTable = data.
-//        }
-//    }
-//}
+            }
+
+            CrystalReport1 report = new CrystalReport1();
+            CrystalReportViewer1.ReportSource = report;
+            ModelDataset ds = GetAllData();
+            report.SetDataSource(ds);
+        }
+
+        public ModelDataset GetAllData()
+        {
+            ModelDataset dataset = new ModelDataset();
+            var headertable = dataset.TransactionHeader;
+            var detailtable = dataset.TransactionDetail;
+
+            List<TransactionHeader> transaction;
+            transaction = db.TransactionHeaders.ToList();
+
+            foreach (TransactionHeader mst in transaction)
+            {
+                var hrow = headertable.NewRow();
+                hrow["TransactionID"] = mst.TransactionID;
+                hrow["TransactionDate"] = mst.TransactionDate;
+                hrow["CustomerID"] = mst.CustomerID;
+                hrow["Status"] = mst.Status;
+                headertable.Rows.Add(hrow);
+
+                foreach (TransactionDetail mstd in mst.TransactionDetails)
+                {
+                    var drow = detailtable.NewRow();
+                    drow["TransactionID"] = mstd.TransactionID;
+                    drow["CardID"] = mstd.CardID;
+                    drow["Quantity"] = mstd.Quantity;
+                    detailtable.Rows.Add(drow);
+                }
+            }
+
+            return dataset;
+        }
+    }
+}
--- LordCardShop/Views/CrystalReport.aspx.designer.cs
+++ LordCardShop/Views/CrystalReport.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace LordCardShop.Views
     {
 
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// CrystalReportViewer1 control.
         /// </summary>
         /// <remarks>
